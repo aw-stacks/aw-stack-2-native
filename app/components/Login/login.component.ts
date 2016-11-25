9eb78cb361b1d15edde06303e383290f15078028
AshwinH <ashwinh@cybage.com>
2016-11-25 19:30:47 +0530

    Update stack

---
+++ app/components/Login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { connectionType, getConnectionType } from "connectivity";
+import { View } from "ui/core/view";
+import { TextField } from "ui/text-field";
+
+@Component({
+  selector: "aw-login",
+  templateUrl: "login.component.html"
+})
+export class LoginComponent {
+
+  // @ViewChild("loginContainer") loginContainer: ElementRef;
+  // @ViewChild("formControls") formControls: ElementRef;
+  // @ViewChild("password") password: ElementRef;
+
+  signin() {
+    alert("You just hit Login");
+  }
+}
