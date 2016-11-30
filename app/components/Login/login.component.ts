6ab064c60f5fc0cd74cd5c71f1bb4e7f06aef3e2
hegdeashwin <ashwin.hegde3@gmail.com>
2016-12-01 03:18:29 +0530

    Update stack

--- app/components/Login/login.component.ts
+++ app/components/Login/login.component.ts
@@ -1,20 +1,23 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
-import { connectionType, getConnectionType } from "connectivity";
-import { View } from "ui/core/view";
-import { TextField } from "ui/text-field";
 
 @Component({
   selector: "aw-login",
   templateUrl: "components/Login/login.component.html"
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  // @ViewChild("loginContainer") loginContainer: ElementRef;
-  // @ViewChild("formControls") formControls: ElementRef;
-  // @ViewChild("password") password: ElementRef;
+  @ViewChild("initialContainer") initialContainer: ElementRef;
+  @ViewChild("mainContainer") mainContainer: ElementRef;
+  @ViewChild("formContainer") formContainer: ElementRef;
 
-  signin() {
-    alert("You just hit Login");
+  constructor(private router: Router) { }
+
+  onSignIn() {
+
+  }
+
+  ngOnInit() {
+
   }
 }
